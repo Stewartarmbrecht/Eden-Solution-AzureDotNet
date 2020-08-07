cc7d89b7cdb7a66646151dfd5bce8ccd9d5a1a09
Stewart Armbrecht <stewart@boundbybetter.com>
2020-08-07 09:27:01 +0000

    Updates to run Testing Code commands on linux.

--- Web/Eden/Testing/Code/Install-ServiceTestCodeTools.ps1
+++ Web/Eden/Testing/Code/Install-ServiceTestCodeTools.ps1
@@ -3,4 +3,41 @@ param(
     [String] $LoggingPrefix
 )
 
-    Write-EdenBuildInfo "You already have everything you need. ;)" $LoggingPrefix
+    $isChromeInstalled = $false
+    Write-EdenBuildInfo "Checking if Chrome is installed." $LoggingPrefix
+    try {
+        google-chrome --version
+        $isChromeInstalled = $true
+        Write-EdenBuildInfo "Chrome is installed." $LoggingPrefix
+    }
+    catch {
+        $isChromeInstalled = $flase
+    }
+    if (!$isChromeInstalled) {
+        Write-EdenBuildInfo "Installing Google Chrome." $LoggingPrefix
+        dpkg -s google-chrome-stable_current_amd64.deb
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo dpkg -i google-chrome-stable_current_amd64.deb
+        sudo apt-get install -f
+        if (Test-Path "google-chrome-stable_current_amd64.deb") {
+            Remove-Item "google-chrome-stable_current_amd64.deb" -Force
+        }
+        Write-EdenBuildInfo "Finished installing Google Chrome." $LoggingPrefix
+    }
+
+    $isLiveServerInstalled = $false
+    Write-EdenBuildInfo "Checking if Live Server is installed." $LoggingPrefix
+    try {
+        live-server --version
+        $isLiveServerInstalled = $true
+        Write-EdenBuildInfo "Live Server is installed." $LoggingPrefix
+    }
+    catch {
+        $isLiveServerInstalled = $false
+    }
+    if (!$isLiveServerInstalled) {
+        Write-EdenBuildInfo "Installing Live Server to run a web server for accessing html files." $LoggingPrefix
+        npm install -g live-server
+        Write-EdenBuildInfo "Finished installing Live Server to run a web server for accessing html files." $LoggingPrefix
+    }
+
--- Web/Eden/Testing/Code/Show-ServiceCodeCoveragePublished.ps1
+++ Web/Eden/Testing/Code/Show-ServiceCodeCoveragePublished.ps1
@@ -4,10 +4,10 @@ param(
     [String] $LoggingPrefix
 )
 
-    Write-EdenBuildInfo "Launching a browser to load the code coverage report at './Reports/Coverage/index.html'." $LoggingPrefix
+    Write-EdenBuildInfo "Launching a browser to load the code coverage report at './Reports/Coverage/'." $LoggingPrefix
 
     Write-Host "" -ForegroundColor Blue
-    Write-Host "Click: http://localhost:8089/index.html" -ForegroundColor Blue
+    Write-Host "Click: http://localhost:8089/" -ForegroundColor Blue
     Write-Host "" -ForegroundColor Blue
     Set-Location "./Reports/Coverage/"
     live-server --port=8089
