347531d67b48b02d0fdb507f6b75b57f3a9f0521
Stewart Armbrecht <stewart@boundbybetter.com>
2020-08-11 01:18:36 +0000

    Updating eden commands.

--- Web/Eden/SourceControl/Show-ServiceChangesLocal.seed.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param(
-    $Settings,
-    [String] $LoggingPrefix
-)
-
-    Write-EdenBuildInfo "Performing some action." $LoggingPrefix
-    # Perform the action here.
-    Write-EdenBuildInfo "Finished performing some action." $LoggingPrefix
