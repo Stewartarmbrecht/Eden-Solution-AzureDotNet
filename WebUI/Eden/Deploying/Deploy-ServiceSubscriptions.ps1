cef10aba30f225955e475028bc15086946f8c7db
Stewart Armbrecht <stewart@boundbybetter.com>
2020-08-24 10:38:52 +0000

    Adding Deploy-ServiceSubscriptions.

---
+++ WebUI/Eden/Deploying/Deploy-ServiceSubscriptions.ps1
@@ -0,0 +1,7 @@
+[CmdletBinding()]
+param(
+    $Settings,
+    [String] $LoggingPrefix
+)
+
+    Write-EdenInfo "There are now subscriptions that need to be deployed for the web application." $LoggingPrefix
