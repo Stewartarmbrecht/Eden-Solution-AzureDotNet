2a47db28c0857da46b42bd201f3fd91762dbac8b
Stewart Armbrecht <stewart@boundbybetter.com>
2020-08-30 11:50:20 +0000

    Removed RemoteLiveReload as it made not changes to the execution.

--- WebUI/Eden/Running/Start-LocalServiceApplication.ps1
+++ WebUI/Eden/Running/Start-LocalServiceApplication.ps1
@@ -6,17 +6,9 @@ param(
 
     Write-EdenInfo "Starting the Angular application with ng serve." $LoggingPrefix
     Set-Location "./App"
-    if ($Settings.RemoteLiveReload.ToLower() -eq "true") {
-        if ($VerbosePreference -eq "SilentlyContinue") {
-            ng serve --open --port=4000 --host 0.0.0.0 | Write-Verbose
-        } else {
-            ng serve --open --port=4000 --host 0.0.0.0
-        }    
+    if ($VerbosePreference -eq "SilentlyContinue") {
+        ng serve --open --port=4000 | Write-Verbose
     } else {
-        if ($VerbosePreference -eq "SilentlyContinue") {
-            ng serve --open --port=4000 | Write-Verbose
-        } else {
-            ng serve --open --port=4000
-        }    
-    }
+        ng serve --open --port=4000
+    }    
     
